31da340e6560296fa5e105b4f4d1ea15e8c28c1b
Bob Souza <souzaw.bruno@gmail.com>
2021-11-19 17:39:31 -0300

    done

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,15 +1,10 @@
-import {
-  useEffect,
-  useRef,
-  useState,
-  useCallback,
-} from 'react';
+import { useEffect, useRef, useState, useCallback } from "react";
 
-import { useField } from '@unform/core';
+import { useField } from "@unform/core";
 
-import { Container } from './styles';
+import { Container } from "./styles";
 
-const Input = ({ name, icon: Icon, ...rest }) => {
+export function Input({ name, icon: Icon, ...rest }: any) {
   const inputRef = useRef(null);
 
   const [isFocused, setIsFocused] = useState(false);
@@ -24,14 +19,15 @@ const Input = ({ name, icon: Icon, ...rest }) => {
   const handleInputBlur = useCallback(() => {
     setIsFocused(false);
 
-    setIsFilled(!!inputRef.current?.value);
+    // setIsFilled(!!inputRef.current?.value);
+    setIsFilled(!!inputRef.current);
   }, []);
 
   useEffect(() => {
     registerField({
       name: fieldName,
       ref: inputRef.current,
-      path: 'value',
+      path: "value",
     });
   }, [fieldName, registerField]);
 
@@ -48,6 +44,6 @@ const Input = ({ name, icon: Icon, ...rest }) => {
       />
     </Container>
   );
-};
+}
 
 export default Input;
---
+++ src/types.ts
@@ -0,0 +1,8 @@
+export interface FoodProps {
+  available: boolean;
+  id: number;
+  description: string;
+  image: string;
+  name: string;
+  price: string;
+}
